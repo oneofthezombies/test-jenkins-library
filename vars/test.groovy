4cab75870a7e55250d77356f6835ccc1ad4262c0
hunhoekim <hunhoekim@gmail.com>
2022-01-20 13:46:21 +0900

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,12 +2,16 @@ import java.lang.Boolean
 
 @NonCPS
 def call() {
-  Boolean.metaClass.toYeAnio {
-    delegate ? '예' : '아니오'
+  Boolean.metaClass.toKorean {
+    delegate ? "예" : "아니오"
   }
 
   def a = true
   println a
   def b = a.toYeAnio()
   println b
+}
+
+def wtf() {
+  "왓더"
 }
