2077395c19abce8736c4596669512c2527bf82f6
oneofthezombies <hunhoekim@gmail.com>
2022-05-07 15:20:30 +0900

    .

---
+++ src/org/db/ProjectA.groovy
@@ -0,0 +1,16 @@
+import org.db.Base
+
+
+class ProjectA extends Base {
+    Integer updateMyProjectVersion(String branch) {
+        Integer version = 0
+        def sql = super.connect()
+        def result = sql.executeInsert """
+            INSERT INTO TblMyProjectVersion (_branch)
+            VALUES (${branch})
+        """, ['_version']
+        getBinding().out.println "db insert reuslt: [${result}]"
+        version = result[0]['GENERATED_KEY']
+        return version
+    }
+}
