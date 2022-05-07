a7e82cc5be36be5c7bb1d2142d2791b78adaf3e8
oneofthezombies <hunhoekim@gmail.com>
2022-05-07 14:16:00 +0900

    .

--- vars/MyProjectDB.groovy
+++ vars/MyProjectDB.groovy
@@ -1,26 +1,24 @@
 import groovy.sql.DataSet
 import groovy.sql.Sql
+import org.jenkinsci.plugins.database.GlobalDatabaseConfiguration
 
 
-static void connect(Closure query) {
-    Map param = [
-        url:'jdbc:mysql://127.0.0.1:3306/test',
-        // user: 'root',
-        // password:'hunhoekim',
-        driver: 'com.mysql.cj.jdbc.Driver'
-    ]
-    Sql.withInstance(param, query)
+static def connect() {
+    def db = GlobalDatabaseConfiguration.get().getDatabase()
+    String jdbcUrl = db.getJdbcUrl()
+    String driverClass = db.getDriverClass()
+    println "DB JDBC URL: [${jdbcUrl}] DriverClass: [${driverClass}]"
+    return new Sql(db.getDataSource())
 }
 
 static Integer updateMyProjectVersion(String branch) {
     Integer version = 0
-    connect({ sql ->
-        def result = sql.executeInsert """
-            INSERT INTO TblMyProjectVersion (_branch)
-            VALUES (${branch})
-        """, ['_version']
-        println result
-        version = result[0]['GENERATED_KEY']
-    })
+    def sql = connect()
+    def result = sql.executeInsert """
+        INSERT INTO TblMyProjectVersion (_branch)
+        VALUES (${branch})
+    """, ['_version']
+    println result
+    version = result[0]['GENERATED_KEY']
     return version
 }
