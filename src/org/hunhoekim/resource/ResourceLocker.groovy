825a650a5ae47364f1bf2e9193f87852bc14a818
oneofthezombies <hunhoekim@gmail.com>
2022-05-21 04:52:00 +0900

    .

--- src/org/hunhoekim/resource/ResourceLocker.groovy
+++ src/org/hunhoekim/resource/ResourceLocker.groovy
@@ -23,21 +23,23 @@ class ResourceLocker implements Serializable {
   @CompileStatic
   class Timeout implements Serializable {
 
+    class Exception extends FlowInterruptedException {
+
+    }
+
     private static final long serialVersionUID = 1
     static final String UNIT_HOURS = 'HOURS'
     static final String UNIT_MINUTES = 'MINUTES'
     static final String UNIT_SECONDS = 'SECONDS'
     static final Integer DEFAULT_TIME = 10
     static final String DEFAULT_UNIT = UNIT_SECONDS
-    Integer time = DEFAULT_TIME
-    String unit = DEFAULT_UNIT
+    static final Integer DEFAULT_RETRY_COUNT = 3
 
   }
 
   private static final long serialVersionUID = 1
   private final CpsScript script
   private Boolean isAcquired = false
-  Integer retryCount = 10
 
   ResourceLocker(CpsScript script) {
     this.script = script
@@ -46,26 +48,39 @@ class ResourceLocker implements Serializable {
   void lock(Map args) {
     List<String> resourceLabels = args['resourceLabels']
     Closure onAcquire = args['onAcquire']
-    Timeout timeout = args.get('timeout', new Timeout(time: Timeout.DEFAULT_TIME, unit: Timeout.DEFAULT_UNIT))
+    Map timeout = args.get('timeout', [:])
+    Integer timeoutTime = timeout.get('time', Timeout.DEFAULT_TIME)
+    String timeoutUnit = timeout.get('unit', Timeout.DEFAULT_UNIT)
+    Integer timeoutRetryCount = timeout.get('retryCount', Timeout.DEFAULT_RETRY_COUNT)
 
-    this.script.parallel(
-      'ResourceLocker acquire step': {
-        this.lockRecursive(resourceLabels, [], onAcquire)
-      },
-      'ResourceLocker timeout step': {
-        try {
-          this.script.timeout(time: timeout.time, unit: timeout.unit) {
-            /* groovylint-disable-next-line EmptyWhileStatement */
-            while (!this.isAcquired) { /* do nothing */ }
-          }
-        } catch (FlowInterruptedException e) {
-          if (!this.isAcquired) {
-            throw e
-          }
-        }
-      },
-      failFast: true
-    )
+    Timeout.Exception lastTimeoutException = null
+    for (Integer i = 0; i < timeoutRetryCount; ++i) {
+      try {
+        this.script.parallel(
+          'ResourceLocker acquire step': {
+            this.lockRecursive(resourceLabels, [], onAcquire)
+          },
+          'ResourceLocker timeout step': {
+            try {
+              this.script.timeout(time: timeoutTime, unit: timeoutUnit) {
+                /* groovylint-disable-next-line EmptyWhileStatement, NestedBlockDepth */
+                while (!this.isAcquired) { /* do nothing */ }
+              }
+            } catch (FlowInterruptedException e) {
+              if (!this.isAcquired) {
+                throw new Timeout.Exception(e)
+              }
+            }
+          },
+          failFast: true
+        )
+        return
+      /* groovylint-disable-next-line CatchException, EmptyCatchBlock */
+      } catch (Timeout.Exception e) {
+        lastTimeoutException = e
+      }
+    }
+    throw lastTimeoutException
   }
 
   private void lockRecursive(
