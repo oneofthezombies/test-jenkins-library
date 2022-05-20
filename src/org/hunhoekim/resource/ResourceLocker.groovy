e193d4b7b85a57b8efbd4f5095dd1480dee71b7d
oneofthezombies <hunhoekim@gmail.com>
2022-05-21 03:39:31 +0900

    .

--- src/org/hunhoekim/resource/ResourceLocker.groovy
+++ src/org/hunhoekim/resource/ResourceLocker.groovy
@@ -1,5 +1,7 @@
 package org.hunhoekim.resource
 
+import groovy.transform.CompileStatic
+import groovy.transform.stc.ClosureParams
 import org.jenkinsci.plugins.workflow.cps.CpsScript
 
 @CompileStatic
